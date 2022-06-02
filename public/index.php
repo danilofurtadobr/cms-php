ca3b28e5b419e68b8f6c44517e004b521b82efc2
Danilo Furtado <65987423+danilofurtadobr@users.noreply.github.com>
2022-06-01 23:16:35 -0300

    Reorganizando pastas do projeto e configurando composer.json

--- public/index.php
+++ public/index.php
@@ -1,6 +1,5 @@
 <?php
 
-// require_once(dirname(__FILE__, 2) . '/config/settings.php');
-// require_once(VIEW_PATH . '/login.php');
 require '../vendor/autoload.php';
 require 'bootstrap.php';
+// require_once(VIEW_PATH . '/login.php');
--- src/config/router.php
+++ src/config/router.php
@@ -11,5 +11,5 @@ function routes()
 function router()
 {
     $uri = parse_url($_SERVER('REQUEST_URI]'), PHP_URL_PATH);
-    echo $uri;
+    var_dump($uri);die;
 }
