af36e2c755210e8295ab67307a9a5761a55b1dcb
Danilo Furtado <65987423+danilofurtadobr@users.noreply.github.com>
2022-06-01 22:02:42 -0300

    Reorganizando pastas do projeto e configurando composer.json

--- public/index.php
+++ public/index.php
@@ -1,4 +1,6 @@
 <?php
 
-require_once(dirname(__FILE__, 2) . '/config/settings.php');
-require_once(VIEW_PATH . '/login.php');
+// require_once(dirname(__FILE__, 2) . '/config/settings.php');
+// require_once(VIEW_PATH . '/login.php');
+require '../vendor/autoload.php';
+require 'bootstrap.php';
