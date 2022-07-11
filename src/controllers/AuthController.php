00eb177217920ed7ec55e6138f1852cb4ba09fff
Danilo Furtado <65987423+danilofurtadobr@users.noreply.github.com>
2022-07-11 13:46:28 -0300

    Retornando user_agent atravez de uma rota

--- src/controllers/AuthController.php
+++ src/controllers/AuthController.php
@@ -56,7 +56,7 @@ class AuthController
     public function apiLogin()
     {
         try {
-            var_dump( $_SERVER['HTTP_USER_AGENT']);
+            var_dump( $_SERVER['HTTP_USER_AGENT'], $this->getClientIp());
             echo ['teste' => '1'];
         } catch(UserException $e) {
             $message = ErrorCodes::translate($e);
@@ -65,5 +65,24 @@ class AuthController
         } catch(\Exception $e) {
             setExceptionMessageAndRedirect('message', ErrorCodes::ERROR_500, '/');
         }
-    }
+    } 
+
+    private function getClientIp() {
+    $ipaddress = '';
+    if (isset($_SERVER['HTTP_CLIENT_IP']))
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    else if(isset($_SERVER['REMOTE_ADDR']))
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
 }
