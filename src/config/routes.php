f300f21e5e4ca3b5ea41a76a6f6f1f20d820de07
Danilo Furtado <65987423+danilofurtadobr@users.noreply.github.com>
2022-07-12 13:07:52 -0300

    Desativando rotas

--- src/config/routes.php
+++ src/config/routes.php
@@ -2,16 +2,16 @@
 
 return [
     'GET' => [
-        '/' => 'DayRecordsController@index',
-        '/login' => 'HomeController@login',
-        '/logout' => 'AuthController@logout',
-        '/user/create' => 'UserController@create',
-        '/user/[a-z0-9]' => 'UserController@index',
-        '/user/[0-9]' => 'UserController@create',
-        '/user/[0-9]+/name/[a-z]+' => 'UserController@show',
+        // '/' => 'DayRecordsController@index',
+        // '/login' => 'HomeController@login',
+        // '/logout' => 'AuthController@logout',
+        // '/user/create' => 'UserController@create',
+        // '/user/[a-z0-9]' => 'UserController@index',
+        // '/user/[0-9]' => 'UserController@create',
+        // '/user/[0-9]+/name/[a-z]+' => 'UserController@show',
     ],
     'POST' => [
-        '/login' => 'AuthController@login',
+        // '/login' => 'AuthController@login',
         '/api/login' => 'AuthController@apiLogin',
     ],
 ];
